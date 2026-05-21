133fb19f7c611cec138dbe7a779f774b97d88cd1
David Esteban Caicedo Saldaña <de.caicedos@gmail.com>
2026-05-21 04:15:31 -0500

    Prueba imagen

--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -43,7 +43,7 @@ export const proyectos: Proyecto[] = [
 		repoURL: 'https://github.com/EstebanC06/CocoTechSystems',
 		// Sube tus capturas a public/assets/CocoTechSystems/ y agrégalas aquí:
 		images: [
-			'/assets/CocoTechSystems/LOGIN1.jpeg',
+			'/assets/HeyFood/heyfood.png',
 			'/assets/CocoTechSystems/REGISTER1.jpeg',
 			'/assets/CocoTechSystems/COCOTECH1.jpeg',
 			'/assets/CocoTechSystems/COCOTECH2.jpeg',
